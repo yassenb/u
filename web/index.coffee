8bd57a32911e80625251484bb811b14359a04615
ngn <nick.nickolov@gmail.com>
2013-05-21 13:16:18 +0300

    .t and .a in the shell show tokens and AST

--- web/index.coffee
+++ web/index.coffee
@@ -1,12 +1,44 @@
-parser = require 'parser'
+{parse} = require 'parser'
+{tokenize} = require 'lexer'
+{compile, exec} = require 'compiler'
 
 jQuery ($) ->
   $('#inp')
     .focus()
     .keypress (event) ->
       if event.keyCode is 13
-        r = try JSON.stringify parser.parse $('#inp').val() catch e then e.stack
-        $('#outp').text $('#outp').text() + '\n' + r
+        uCode = $('#inp').val()
+        if not uCode then return false
+        $('#outp').text $('#outp').text() + '\n' + (
+          try
+            if /^\.t\b/.test uCode
+              tokenStream = tokenize uCode[3...]
+              "Tokens for #{JSON.stringify uCode}:\n  #{
+                (
+                  while (t = tokenStream.next()).type isnt 'eof'
+                    JSON.stringify t
+                ).join '\n  '
+              }"
+            else if /^\.a\b/.test uCode
+              "AST for #{JSON.stringify uCode}:\n#{
+                renderAST parse uCode[3...]
+              }"
+            else if /^\.c\b/.test uCode
+              compile uCode[3...]
+            else
+              exec uCode
+          catch e
+            e.stack
+        )
         $(window).scrollTop $(document).height()
         false
       true
+
+renderAST = (node, indent = '  ') ->
+  if node.length is 2 and typeof node[1] is 'string'
+    indent + node[0] + ' ' + JSON.stringify node[1]
+  else
+    indent + node[0] + '\n' + (
+      for child in node[1...]
+        renderAST child, indent + '  '
+    ).join '\n'
--- web/index.js
+++ web/index.js
@@ -1,27 +1,74 @@
 (function() {
-  var parser;
+  var compile, exec, parse, renderAST, tokenize, _ref;
 
-  parser = require('parser');
+  parse = require('parser').parse;
+
+  tokenize = require('lexer').tokenize;
+
+  _ref = require('compiler'), compile = _ref.compile, exec = _ref.exec;
 
   jQuery(function($) {
     return $('#inp').focus().keypress(function(event) {
-      var e, r;
+      var e, t, tokenStream, uCode;
 
       if (event.keyCode === 13) {
-        r = (function() {
+        uCode = $('#inp').val();
+        if (!uCode) {
+          return false;
+        }
+        $('#outp').text($('#outp').text() + '\n' + ((function() {
           try {
-            return JSON.stringify(parser.parse($('#inp').val()));
+            if (/^\.t\b/.test(uCode)) {
+              tokenStream = tokenize(uCode.slice(3));
+              return "Tokens for " + (JSON.stringify(uCode)) + ":\n  " + (((function() {
+                var _results;
+
+                _results = [];
+                while ((t = tokenStream.next()).type !== 'eof') {
+                  _results.push(JSON.stringify(t));
+                }
+                return _results;
+              })()).join('\n  '));
+            } else if (/^\.a\b/.test(uCode)) {
+              return "AST for " + (JSON.stringify(uCode)) + ":\n" + (renderAST(parse(uCode.slice(3))));
+            } else if (/^\.c\b/.test(uCode)) {
+              return compile(uCode.slice(3));
+            } else {
+              return exec(uCode);
+            }
           } catch (_error) {
             e = _error;
             return e.stack;
           }
-        })();
-        $('#outp').text($('#outp').text() + '\n' + r);
+        })()));
         $(window).scrollTop($(document).height());
         false;
       }
       return true;
     });
   });
+
+  renderAST = function(node, indent) {
+    var child;
+
+    if (indent == null) {
+      indent = '  ';
+    }
+    if (node.length === 2 && typeof node[1] === 'string') {
+      return indent + node[0] + ' ' + JSON.stringify(node[1]);
+    } else {
+      return indent + node[0] + '\n' + ((function() {
+        var _i, _len, _ref1, _results;
+
+        _ref1 = node.slice(1);
+        _results = [];
+        for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
+          child = _ref1[_i];
+          _results.push(renderAST(child, indent + '  '));
+        }
+        return _results;
+      })()).join('\n');
+    }
+  };
 
 }).call(this);
